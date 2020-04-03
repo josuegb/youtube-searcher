154ca6c6dcf01a524c8a079ed2206a39d8c6d62b
josuegb <josuegb15@gmail.com>
2020-04-03 02:37:09 -0600

    Update key

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var API_KEY = 'AIzaSyBeMoVU5Wjeg_0mYd5e6BjmVg7hHY60vDc';
+	var API_KEY = 'AIzaSyDdYBQCVsytlaBgQU5njmBdHHxGT3cZd_Y';
 
 	var App = function (_Component) {
 	    _inherits(App, _Component);
